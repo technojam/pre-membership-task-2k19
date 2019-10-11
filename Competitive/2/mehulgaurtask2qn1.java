3d38543796d066bfb11eec1e9927e19c9bacebbd
malignent-MG <56211612+malignent-MG@users.noreply.github.com>
2019-10-11 11:41:23 -0700

    Create mehulgaurtask2qn1.java

---
+++ Competitive/2/mehulgaurtask2qn1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        
+        int k=1;
+        int m1[][]= new int[n][n];
+        String mat[][]=new String[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for (int j = 0; j <(n); j++) {
+                if(!(i==j) && j<i)
+                {
+                    mat[i][j]="**";
+                }
+                if(j>=i &&j<=4)
+                {
+                   mat[i][j]=Integer.toString(k*10);
+                    k++;
+                }
+               
+            } 
+            
+        }
+        int p=1;
+        for (int i = n-1; i>=0; i--) {
+            for (int j = 0; j < p; j++) {
+                m1[i][j]=(k*10);
+                k++;
+                
+            }
